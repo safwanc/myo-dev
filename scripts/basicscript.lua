d1ba38bebea3d525ae6a604ecd538e48da5cf508
Safwan Choudhury <im@safwanc.com>
2014-10-22 19:08:17 -0400

    Implemented onPoseEdge(..)

--- scripts/basicscript.lua
+++ scripts/basicscript.lua
@@ -4,3 +4,15 @@ function onForegroundWindowChange(app, title)
 	myo.debug("onForegroundWindowChange app: " .. app .. " title: " .. title)
 	return true
 end
+
+function onPoseEdge(pose, edge)
+	myo.debug("onPoseEdge: " .. pose .. ": " .. edge)
+
+	if (pose == "thumbToPinky") then
+		customFunction()
+	end
+end
+
+function customFunction()
+	myo.debug("Custom Function Fired!")
+end
